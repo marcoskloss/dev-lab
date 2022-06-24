4b628cc774140975eee6780fe682006a47eef3d4
Marcos Kloss <marcoskloss2001@gmail.com>
2022-06-24 09:01:35 -0300

    server: login endpoint

--- web-misc/angular/auth/server/app.js
+++ web-misc/angular/auth/server/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
 const { v4: uuid } = require("uuid");
 const bcrypt = require("bcrypt");
+
+const JWT_SECRET = "mayTheForceBeWithYou";
 
 const app = express();
 app.use(express.json());
@@ -29,6 +32,29 @@ app.post("/register", async (req, res) => {
 
   db[user.id] = user;
   res.status(201).send({ user });
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).send({ message: "credentials not provided" });
+
+  const [, user] =
+    Object.entries(db).find((keyValue) => {
+      return keyValue[1].email === email;
+    }) || [];
+
+  if (!user) {
+    return res.status(404).send({ message: "user not found" });
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.status(404).send({ message: "user not found" });
+  }
+
+  const token = jwt.sign({ sub: user.id }, JWT_SECRET);
+  return res.send({ user, token });
 });
 
 app.listen(3333, () => {
