1548cc8dfbb023e3ab6333076e0c51ba196a6b46
Marcos Kloss <marcoskloss2001@gmail.com>
2022-10-08 10:03:54 -0300

    rust - chapter2

--- rust/chapter-2/guessing_game/src/main.rs
+++ rust/chapter-2/guessing_game/src/main.rs
@@ -6,24 +6,31 @@ fn main() {
     println!("Guess the number!");
     
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
 
-    println!("Input your guess.");
+    loop {
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        println!("Input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Equal => println!("You win :D"),
-        Ordering::Less => println!("Too small :/"),
-        Ordering::Greater => println!("Too big :/"),
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+    
+        println!("You guessed: {guess}");
+    
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small :/"),
+            Ordering::Greater => println!("Too big :/"),
+            Ordering::Equal => {
+                println!("You win :D");
+                break;
+            },
+        }
     }
 }
     
