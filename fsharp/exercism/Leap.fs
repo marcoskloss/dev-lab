1183796320c2d94c2d998ba162df1abc3b5d0267
Marcos Kloss <marcoskloss2001@gmail.com>
2023-03-02 18:02:44 -0300

    removing old fsharp exercism exercises

--- fsharp/exercism/Leap.fs
+++
@@ -1,8 +0,0 @@
-module Leap
-
-let leapYear (year: int): bool =
-    year % 4 = 0 && (year % 100 <> 0 || year % 400 = 0)
-
-let leapYear_otherCoolSolution (year: int): bool =
-    let divisibleBy x = (year % x) = 0
-    divisibleBy 4 && (not (divisibleBy 100) || divisibleBy 400)
