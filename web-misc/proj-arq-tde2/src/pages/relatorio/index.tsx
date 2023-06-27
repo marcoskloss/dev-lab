eef57edec1da4ca02cca009f3d654cb4a007f575
Marcos Kloss <mkloss@rd.com.br>
2023-06-27 13:52:23 -0300

    report table

--- web-misc/proj-arq-tde2/src/pages/relatorio/index.tsx
+++ web-misc/proj-arq-tde2/src/pages/relatorio/index.tsx
@@ -1,8 +1,20 @@
-import { Button } from "@chakra-ui/react"
+import { Button, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react"
 import { Layout } from "../../components/layout"
-import { getCsvReport } from "../../services/report"
+import { getCsvReport, getTableReport } from "../../services/report"
+import { useEffect, useState } from "react"
+import { type Universidade } from "../universidade"
+import { type Curso } from "../curso"
+import { type Aluno } from "../aluno"
+
+type TableData = {
+  student: Aluno;
+  university: Universidade;
+  course: Curso
+}
 
 export function Relatorio() {
+  const [data, setData] = useState<TableData[]>([])
+  
   const downloadReport = async () => {
     const csv = await getCsvReport();
     const csvBlob = new Blob([csv], { type: 'text/csv' });
@@ -16,9 +28,33 @@ export function Relatorio() {
     window.URL.revokeObjectURL(url);
   }
 
+  useEffect(() => {
+    getTableReport().then(setData);
+  }, [])
+
   return (
-    <Layout title="Relatório">
-      <Button onClick={downloadReport}>Gerar</Button>
+    <Layout title="Relatório" headerAction={<Button ml='auto' colorScheme="blue" onClick={downloadReport}>Baixar relatório</Button>}>
+      <TableContainer>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>Aluno</Th>
+              <Th>Curso</Th>
+              <Th>Universidade</Th>
+            </Tr>
+          </Thead>
+
+          <Tbody>
+            {data.map(row => (
+              <Tr>
+                <Td>{row.student.nome}</Td>
+                <Td>{row.course.nome}</Td>
+                <Td>{row.university.nome}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Layout>
   )
 }
--- web-misc/proj-arq-tde2/src/services/report.ts
+++ web-misc/proj-arq-tde2/src/services/report.ts
@@ -40,3 +40,36 @@ export const getCsvReport = async () => {
 
   return rows.join('\n');
 }
+
+export const getTableReport = async () => {
+  const rows = [];
+
+  const students = await getAllStudents();
+
+  const coursesById = new Map<number, Curso>();
+  const universitiesById = new Map<number, Universidade>();
+
+  for await (const student of students) {
+    let course: Curso;
+    let university: Universidade;
+
+    if (coursesById.has(student.curso_id)) {
+      course = coursesById.get(student.curso_id) as Curso;
+    } else {
+      course = await getCourseById(student.curso_id);
+      coursesById.set(course.id, course);
+    }
+
+    if (universitiesById.has(course.universidade_id)) {
+      university = universitiesById.get(course.universidade_id) as Universidade;
+    } else {
+      university = await getUniversityById(course.universidade_id);
+      universitiesById.set(university.id, university);
+    }
+    
+
+    rows.push({ student, course, university });
+  }
+
+  return rows;
+}
